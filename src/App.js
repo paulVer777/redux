adfa9515e141ab1081aead919d86ef5dbad7203c
paulVer777 <pawel.gitara@onet.eu>
2018-05-14 10:58:01 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+    render() {
+        return (
+
+
+            <div>
+
+
+                redux
+
+
+            </div>
+        );
+    }
+}
+
+export default App;
